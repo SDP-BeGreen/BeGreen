25c2c493b946414cc8f038c0913bdb5e8666492f
Xavier <xavier.ogay@epfl.ch>
2023-03-03 08:49:16 +0100

    Working BoredAPI

--- app/src/main/java/com/github/sdp_begreen/begreen/BoredActivity.kt
+++ app/src/main/java/com/github/sdp_begreen/begreen/BoredActivity.kt
@@ -3,5 +3,4 @@ package com.github.sdp_begreen.begreen
 data class BoredActivity(
     // Check the response format of the response! See the documentation
     val activity: String,
-
 )
--- app/src/main/java/com/github/sdp_begreen/begreen/MainActivity.kt
+++ app/src/main/java/com/github/sdp_begreen/begreen/MainActivity.kt
@@ -5,7 +5,7 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 import android.widget.TextView
-import retrofit2.Retrofit
+import retrofit2.*
 import retrofit2.converter.gson.GsonConverterFactory
 
 class MainActivity : AppCompatActivity() {
@@ -20,11 +20,47 @@ class MainActivity : AppCompatActivity() {
             intent.putExtra("name", textVal.text.toString())
             startActivity(intent)
         }
+
+        //------------Retrofit Bootcamp--------------------------
         //baseUrl of the API and the addConverterFactory will convert the response
         //to a BoredActivity Object
         val retrofit = Retrofit.Builder()
             .baseUrl("https://www.boredapi.com/api/")
             .addConverterFactory(GsonConverterFactory.create())
             .build()
+        //instance of boredApi
+        val boredApi = retrofit.create(BoredApi::class.java)
+
+
+
+
+
+        val btnConnect : Button = findViewById(R.id.create_api)
+        val btnSen : Button = findViewById(R.id.send_request)
+        val textResponse : TextView = findViewById(R.id.text_response)
+        btnSen.setOnClickListener {
+            boredApi.getActivity().enqueue(
+                object : Callback<BoredActivity>{
+                    override fun onResponse(call: Call<BoredActivity>, response: Response<BoredActivity>
+                    ) {
+                        if(response.isSuccessful){
+                            textResponse.text = response.body()?.activity
+                        }else{
+                            textResponse.text = response.errorBody().toString()
+                        }
+
+                    }
+
+                    override fun onFailure(call: Call<BoredActivity>, t: Throwable) {
+                        textResponse.text = t.message.toString()
+                    }
+                }
+            )
+        }
+
+
+        btnConnect.setOnClickListener{
+
+        }
     }
 }
