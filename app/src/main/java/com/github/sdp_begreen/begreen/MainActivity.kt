a02850fba95450ad477a0eb0530f84250822f28e
LucaCarroz <71666958+LucaCarroz@users.noreply.github.com>
2023-03-03 16:39:46 +0100

    Firebase (#10)
    Added new activity that can be used to communicate with firebase's real time database, with complete tests and doc.

--- app/src/main/java/com/github/sdp_begreen/begreen/MainActivity.kt
+++ app/src/main/java/com/github/sdp_begreen/begreen/MainActivity.kt
@@ -1,10 +1,10 @@
 package com.github.sdp_begreen.begreen
 
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -13,10 +13,17 @@ class MainActivity : AppCompatActivity() {
 
         val btn: Button = findViewById(R.id.mainButton)
         btn.setOnClickListener {
-            val textVal: TextView = findViewById(R.id.mainName);
+            val textVal: TextView = findViewById(R.id.mainName)
             val intent = Intent(this, GreetingActivity::class.java)
             intent.putExtra("name", textVal.text.toString())
             startActivity(intent)
         }
+
+        val btnDB: Button = findViewById(R.id.mainQuery)
+        btnDB.setOnClickListener {
+            val intent = Intent(this, DatabaseActivity::class.java)
+            startActivity(intent)
+        }
+
     }
 }
