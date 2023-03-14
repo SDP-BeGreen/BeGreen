71320ab04fc2118001884f823318ecd1df380f37
LucaCarroz <71666958+LucaCarroz@users.noreply.github.com>
2023-03-14 11:27:21 +0100

    Picture database on firebase
    Added interface to store and retrieve images from Firebase relatime database. Also set up Firebase's Emulator to be used for testing.

--- app/src/androidTest/java/com/github/sdp_begreen/begreen/activities/DatabaseActivityTest.kt
+++ app/src/androidTest/java/com/github/sdp_begreen/begreen/activities/DatabaseActivityTest.kt
@@ -8,39 +8,28 @@ import androidx.test.espresso.matcher.ViewMatchers.*
 import androidx.test.ext.junit.rules.ActivityScenarioRule
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import androidx.test.filters.LargeTest
-import com.github.sdp_begreen.begreen.Database
+import com.github.sdp_begreen.begreen.FirebaseDB
 import com.github.sdp_begreen.begreen.R
+import com.google.firebase.database.ktx.database
+import com.google.firebase.ktx.Firebase
+import org.junit.After
 import org.junit.Before
 import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
-import java.util.concurrent.CompletableFuture
+
 
 @RunWith(AndroidJUnit4::class)
 @LargeTest
 class DatabaseActivityTest {
-    @get:Rule
-    val activityRule = ActivityScenarioRule(DatabaseActivity::class.java)
-
-    /*
-    Implementation of a local database
-     */
-    class MockDataBase : Database() {
-        private val map: HashMap<String, String> = HashMap()
-        override fun get(key: String): CompletableFuture<String> {
-            return CompletableFuture.completedFuture(map[key])
-        }
-
-        override fun set(key: String, value: String) {
-            map[key] = value
-        }
-    }
 
     @Before
-    fun before() {
-        // Change the used database to the local database
-        Database.db = MockDataBase()
+    fun init() {
+        Firebase.database.useEmulator("10.0.2.2",9000)
     }
+
+    @get:Rule
+    val activityRule = ActivityScenarioRule(DatabaseActivity::class.java)
 
     @Test
     fun emailWrittenCorrectly() {
@@ -57,10 +46,27 @@ class DatabaseActivityTest {
     }
 
     @Test
-    fun getWithInexistantKeyReturnsNothing() {
+    fun getWithInexistantKeyPrintsNothing() {
         // Type phone number
         onView(withId(R.id.databasePhoneNumber))
             .perform(typeText("1"))
+            .perform(closeSoftKeyboard())
+
+
+        // Press get button
+        onView(withId(R.id.databaseGet))
+            .perform(click())
+        // Check that no email is linked to that phone number
+        onView(withId(R.id.databaseEmail))
+            .check(matches(withText("")))
+    }
+
+    @Test
+    fun getOnNonStringValuePrintsNothing() {
+        // Type phone number
+        onView(withId(R.id.databasePhoneNumber))
+            .perform(typeText("123"))
+            .perform(closeSoftKeyboard())
         // Press get button
         onView(withId(R.id.databaseGet))
             .perform(click())
@@ -73,16 +79,31 @@ class DatabaseActivityTest {
     fun setCorrectlyUpdatesDatabase() {
         // Set value "email@example.com" for key 123
         onView(withId(R.id.databasePhoneNumber))
-            .perform(typeText("123"))
+            .perform(typeText("123456"))
+            .perform(closeSoftKeyboard())
         onView(withId(R.id.databaseEmail))
             .perform(typeText("email@example.com"))
+            .perform(closeSoftKeyboard())
         onView(withId(R.id.databaseSet))
             .perform(click())
+
+        // Clear the e-mail
+        onView(withId(R.id.databaseEmail))
+            .perform(clearText())
+            .perform(closeSoftKeyboard())
 
         // Check that value return by "get" for key 123 is "email@example.com"
         onView(withId(R.id.databaseGet))
             .perform(click())
         onView(withId(R.id.databaseEmail))
             .check(matches(withText("email@example.com")))
+    }
+
+    @Test
+    fun storeFollowedByLoadCorrectlyDisplaysBitmap() {
+        onView(withId(R.id.databaseStorePicture))
+            .perform(click())
+        onView(withId(R.id.databaseLoadPicture))
+            .perform(click())
     }
 }
--- app/src/main/java/com/github/sdp_begreen/begreen/activities/DatabaseActivity.kt
+++ app/src/main/java/com/github/sdp_begreen/begreen/activities/DatabaseActivity.kt
@@ -1,13 +1,23 @@
 package com.github.sdp_begreen.begreen.activities
 
+import android.graphics.Bitmap
+import android.graphics.Color
 import android.os.Bundle
 import android.widget.Button
 import android.widget.EditText
+import android.widget.ImageView
 import androidx.appcompat.app.AppCompatActivity
-import com.github.sdp_begreen.begreen.Database.Companion.db
+import androidx.lifecycle.lifecycleScope
+import com.github.sdp_begreen.begreen.FirebaseDB.Companion.db
 import com.github.sdp_begreen.begreen.R
+import kotlinx.coroutines.launch
 
+/**
+ * This file will be deleted when Firebase realtime database gets merged in the project
+ */
 class DatabaseActivity : AppCompatActivity() {
+
+    private var imageId: String? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -24,10 +34,42 @@ class DatabaseActivity : AppCompatActivity() {
         // Get function
         val getBtn: Button = findViewById(R.id.databaseGet)
         getBtn.setOnClickListener {
-            db[phoneText.text.toString()].thenAccept {
-                emailText.setText(it)
+            /*db[phoneText.text.toString()].thenAccept {
+                if (it != null) emailText.setText(it)
+            }*/
+            lifecycleScope.launch {
+                db.get(phoneText.text.toString())?.also { emailText.setText(it) }
             }
         }
 
+        val storePictureBtn: Button = findViewById(R.id.databaseStorePicture)
+        storePictureBtn.setOnClickListener {
+
+            val size = 100 // size of the square in pixels
+            val bitmap = Bitmap.createBitmap(size, size, Bitmap.Config.ARGB_8888)
+            bitmap.eraseColor(Color.RED)
+
+            imageId = db.addImage(bitmap, 3)
+
+        }
+
+        val getPictureBtn: Button = findViewById(R.id.databaseLoadPicture)
+        getPictureBtn.setOnClickListener {
+
+            /*imageId?.also {
+
+                db.getImage(it, 3).thenAccept {bitmap ->
+                    val image: ImageView = findViewById(R.id.databasePicture)
+                    image.setImageBitmap(bitmap)
+                }
+            }*/
+            lifecycleScope.launch {
+                imageId?.also {
+                    db.getImage(it, 3).also { bitmap ->
+                        findViewById<ImageView>(R.id.databasePicture).setImageBitmap(bitmap)
+                    }
+                }
+            }
+        }
     }
 }
