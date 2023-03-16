faeffecce98a7c492e97aa7a04c15d4656ee7e06
Bryan Porcello <bryan.porcello@epfl.ch>
2023-03-16 15:48:22 +0100

    Final version of Google Sign in integration with Firebase to the current project.

---
+++ app/src/main/java/com/github/sdp_begreen/begreen/social/GoogleAuth.kt
@@ -0,0 +1,45 @@
+package com.github.sdp_begreen.begreen.social
+
+import android.content.Context
+import android.content.Intent
+import com.github.sdp_begreen.begreen.R
+import com.google.android.gms.auth.api.signin.GoogleSignIn
+import com.google.android.gms.auth.api.signin.GoogleSignInClient
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions
+
+
+
+object GoogleAuth {
+
+    // GoogleSignInClient object that can be used to initiate the Google sign-in process in an Android app
+    // This variable will hold the configured GoogleSignInClient object.
+    // Source : https://developers.google.com/android/reference/com/google/android/gms/auth/api/signin/GoogleSignInClient
+    lateinit var mGoogleSignInClient: GoogleSignInClient
+
+    fun googleClient(context: Context) {
+        // Configure sign-in to request the user's ID, email address, and basic
+        // profile. ID and basic profile are included in DEFAULT_SIGN_IN.
+        val gso = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+            .requestIdToken(context.getString(R.string.default_web_client_id))
+            .requestEmail()
+            .build()
+
+        // Create a GoogleSignInClient object to interact with the Google Sign-In API
+        mGoogleSignInClient = GoogleSignIn.getClient(context, gso)
+    }
+
+
+    fun googleSignIn() : Intent {
+        val signInIntent: Intent = mGoogleSignInClient.signInIntent
+        return signInIntent
+    }
+
+
+    fun googleLogOut(context: Context, logoutCallback: (Context) -> Unit) {
+        googleClient(context)
+        mGoogleSignInClient.signOut()
+        logoutCallback(context)
+    }
+
+}
+
