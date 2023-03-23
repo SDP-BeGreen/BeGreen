a5747331e44f5e9c7a715bf1b343d02d316b7bac
Bryan Porcello <bryan.porcello@epfl.ch>
2023-03-23 16:19:34 +0100

    Final commit of the TipsFeature without the required tests.

--- app/src/main/java/com/github/sdp_begreen/begreen/fragments/AdviceFragment.kt
+++ app/src/main/java/com/github/sdp_begreen/begreen/fragments/AdviceFragment.kt
@@ -7,11 +7,7 @@ import android.view.ViewGroup
 import android.widget.TextView
 import androidx.fragment.app.Fragment
 import com.github.sdp_begreen.begreen.R
-
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
+import kotlin.random.Random
 
 /**
  * A simple [Fragment] subclass.
@@ -20,51 +16,38 @@ private const val ARG_PARAM2 = "param2"
  */
 class AdviceFragment : Fragment() {
     // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
+    private lateinit var adviceFragmentTextView: TextView
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
-    }
+    // This will hold the pieces of advice that will be displayed in the TextView
+    private var adviceList= ArrayList<String>()
 
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        val view: View? = inflater.inflate(R.layout.fragment_advice, container, false)
-        val tView: TextView? = view?.findViewById(R.id.adviceFragmentTextView)
+    // The purpose of this method is to inflate the fragment layout, initialize some views,
+    // and display a random piece of advice on the screen.
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
+                              savedInstanceState: Bundle?): View {
+        val view = inflater.inflate(R.layout.fragment_advice, container, false)!!
 
-        tView?.text = getString(
-            R.string.advice_fragment_text,
-            param1?.let { " $it" }.orEmpty(),
-            param2?.let { ", $it" }.orEmpty()
-        )
+        adviceFragmentTextView = view.findViewById(R.id.adviceFragmentTextView)
+
+        addData()
+
+        // Sets the text of the adviceFragmentTextView to a random string from the adviceList variable.
+        adviceFragmentTextView.text = adviceList.get(Random.nextInt(adviceList.size))
 
         return view
     }
-
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment AdviceFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            AdviceFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
+    // Helper method to add some advice strings to the adviceList variable
+    // TODO: insert a random generator of advices if possible
+    private fun addData() {
+        adviceList.add("Ecology helps us to understand how our actions affect the environment. It shows the individuals the extent of damage we cause to the environment.")
+        adviceList.add("With the knowledge of ecology, we are able to know which resources are necessary for the survival of different organisms. Lack of ecological knowledge has led to scarcity and deprivation of these resources, leading to competition.")
+        adviceList.add("All organisms require energy for their growth and development. Lack of ecological understanding leads to the over-exploitation of energy resources such as light, nutrition and radiation, leading to its depletion.")
+        adviceList.add("Ecology encourages harmonious living within the species and the adoption of a lifestyle that protects the ecology of life.")
+        adviceList.add("It focuses on the relationship between humans and the environment. It emphasizes the impact human beings have on the environment and gives knowledge on how we can improve ourselves for the betterment of humans and the environment.")
+        adviceList.add("It deals with the study of how organisms alter the environment for the benefit of themselves and other living beings. For eg, termites create a 6 feet tall mound and at the same time feed and protect their entire population.")
+        adviceList.add("Ecology plays a significant role in forming new species and modifying the existing ones. Natural selection is one of the many factors that influences evolutionary change.")
+        adviceList.add("Ecology was first devised by Ernst Haeckel, a German Zoologist. However, ecology has its origins in other sciences such as geology, biology, and evolution among others.")
+        adviceList.add("Habitat ecology is the type of natural environment in which a particular species of an organism live, characterized by both physical and biological features.")
+        adviceList.add("An organism free from the interference of other species and can use a full range of biotic and abiotic resources in which it can survive and reproduce is known as its fundamental niche.")
     }
 }
