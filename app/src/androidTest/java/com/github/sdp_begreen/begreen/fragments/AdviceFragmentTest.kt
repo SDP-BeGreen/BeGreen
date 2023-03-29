0bdbba58d62b84ef4dd51c7c7bde9038f81f22ef
Bryan Porcello <bryan.porcello@epfl.ch>
2023-03-29 11:24:27 +0200

    Test corrected and AdviceFragment modified to use a companion object

--- app/src/androidTest/java/com/github/sdp_begreen/begreen/fragments/AdviceFragmentTest.kt
+++ app/src/androidTest/java/com/github/sdp_begreen/begreen/fragments/AdviceFragmentTest.kt
@@ -2,48 +2,25 @@ package com.github.sdp_begreen.begreen.fragments
 
 import android.os.Bundle
 import androidx.fragment.app.testing.launchFragmentInContainer
-import androidx.test.espresso.Espresso
-import androidx.test.espresso.action.ViewActions.typeText
+import androidx.test.espresso.Espresso.onView
 import androidx.test.espresso.assertion.ViewAssertions.matches
+import androidx.test.espresso.matcher.ViewMatchers.*
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import androidx.test.filters.LargeTest
 import com.github.sdp_begreen.begreen.R
 import org.junit.Test
 import org.junit.runner.RunWith
-import androidx.test.espresso.Espresso.onView
-import androidx.test.espresso.matcher.ViewMatchers.*
-import org.hamcrest.CoreMatchers
 
 @RunWith(AndroidJUnit4::class)
 @LargeTest
 class AdviceFragmentTest {
     @Test
     fun testAdviceFragment() {
-        launchFragmentInContainer<AdviceFragment>()
+        val QUOTES = "quotes"
+        launchFragmentInContainer<AdviceFragment>(Bundle().apply {
+                putStringArrayList(QUOTES, arrayListOf("Hello World!"))
+        })
         onView(withId(R.id.adviceFragmentTextView))
-            .check(matches(withText("Fragment where we can display ecological advice to the user")))
-    }
-
-
-    @Test
-    fun checkTextViewIsEmpty() {
-        onView(withId(R.id.adviceFragmentTextView))
-            .check(matches(withText(""))) // checks if the text view is empty
-    }
-
-    @Test
-    fun checkStringLength() {
-        val maxLength = 100
-        val stringToBeMatched = "Hello World!"
-        onView(withId(R.id.adviceFragmentTextView))
-            .check(matches(withText(CoreMatchers.not(CoreMatchers.containsString(stringToBeMatched.substring(maxLength))))))
-    }
-
-
-    @Test
-    fun checkText() {
-        val expectedText = "Hello, world!"
-        onView(withId(R.id.adviceFragmentTextView)).perform(typeText(expectedText)) // enters text into the text view
-        onView(withId(R.id.adviceFragmentTextView)).check(matches(withText(expectedText))) // retrieves the text from the text view
+            .check(matches(withText("Hello World!")))
     }
 }
--- app/src/main/java/com/github/sdp_begreen/begreen/fragments/AdviceFragment.kt
+++ app/src/main/java/com/github/sdp_begreen/begreen/fragments/AdviceFragment.kt
@@ -16,38 +16,41 @@ import kotlin.random.Random
  */
 class AdviceFragment : Fragment() {
 
-    private lateinit var adviceFragmentTextView: TextView
-
-    // This will hold the pieces of advice that will be displayed in the TextView
-    private var adviceList= ArrayList<String>()
-
     // The purpose of this method is to inflate the fragment layout, initialize some views,
     // and display a random piece of advice on the screen.
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View {
+
         val view = inflater.inflate(R.layout.fragment_advice, container, false)!!
 
-        adviceFragmentTextView = view.findViewById(R.id.adviceFragmentTextView)
-
-        addData()
+        val adviceFragmentTextView = view.findViewById<TextView>(R.id.adviceFragmentTextView)
 
         // Sets the text of the adviceFragmentTextView to a random string from the adviceList variable.
-        adviceFragmentTextView.text = adviceList.get(Random.nextInt(adviceList.size))
+        arguments?.also { it.getStringArrayList(QUOTES)?.also { list ->  adviceFragmentTextView.text =
+            list[Random.nextInt(list.size)]
+        } }
 
         return view
     }
-    // Helper method to add some advice strings to the adviceList variable
-    // TODO: insert a random generator of advices if possible
-    private fun addData() {
-        adviceList.add("Ecology helps us to understand how our actions affect the environment. It shows the individuals the extent of damage we cause to the environment.")
-        adviceList.add("With the knowledge of ecology, we are able to know which resources are necessary for the survival of different organisms. Lack of ecological knowledge has led to scarcity and deprivation of these resources, leading to competition.")
-        adviceList.add("All organisms require energy for their growth and development. Lack of ecological understanding leads to the over-exploitation of energy resources such as light, nutrition and radiation, leading to its depletion.")
-        adviceList.add("Ecology encourages harmonious living within the species and the adoption of a lifestyle that protects the ecology of life.")
-        adviceList.add("It focuses on the relationship between humans and the environment. It emphasizes the impact human beings have on the environment and gives knowledge on how we can improve ourselves for the betterment of humans and the environment.")
-        adviceList.add("It deals with the study of how organisms alter the environment for the benefit of themselves and other living beings. For eg, termites create a 6 feet tall mound and at the same time feed and protect their entire population.")
-        adviceList.add("Ecology plays a significant role in forming new species and modifying the existing ones. Natural selection is one of the many factors that influences evolutionary change.")
-        adviceList.add("Ecology was first devised by Ernst Haeckel, a German Zoologist. However, ecology has its origins in other sciences such as geology, biology, and evolution among others.")
-        adviceList.add("Habitat ecology is the type of natural environment in which a particular species of an organism live, characterized by both physical and biological features.")
-        adviceList.add("An organism free from the interference of other species and can use a full range of biotic and abiotic resources in which it can survive and reproduce is known as its fundamental niche.")
+
+
+    companion object {
+
+        private val QUOTES = "quotes"
+        /**
+         * Use this factory method to create a new instance of
+         * this fragment using the provided parameters.
+         *
+         * @param param1 Parameter 1.
+         * @return A new instance of fragment FavoriteFragment.
+         */
+        // TODO: Rename and change types and number of parameters
+        @JvmStatic
+        fun newInstance(param1: ArrayList<String>) =
+            AdviceFragment().apply {
+                arguments = Bundle().apply {
+                    putStringArrayList(QUOTES, param1)
+                }
+            }
     }
 }
