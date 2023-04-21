3339d9a7156ca05e95c3ef0dfc86e09be0351371
Luca <luca.carroz@epfl.ch>
2023-04-21 19:30:30 +0200

    Resolve merge conflict

--- app/src/androidTest/java/com/github/sdp_begreen/begreen/fragments/AdviceFragmentTest.kt
+++ app/src/androidTest/java/com/github/sdp_begreen/begreen/fragments/AdviceFragmentTest.kt
@@ -1,6 +1,5 @@
 package com.github.sdp_begreen.begreen.fragments
 
-import androidx.fragment.app.testing.launchFragmentInContainer
 import androidx.test.espresso.Espresso.onView
 import androidx.test.espresso.assertion.ViewAssertions.matches
 import androidx.test.espresso.matcher.ViewMatchers.*
@@ -8,16 +7,17 @@ import androidx.test.ext.junit.runners.AndroidJUnit4
 import androidx.test.filters.LargeTest
 import com.github.sdp_begreen.begreen.R
 import com.github.sdp_begreen.begreen.firebase.DB
-import com.github.sdp_begreen.begreen.matchers.ContainsStringFromCollectionMatcher.Companion.hasStringFromCollection
 import com.github.sdp_begreen.begreen.rules.KoinTestRule
-import kotlinx.coroutines.runBlocking
-import kotlinx.coroutines.withTimeout
 import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
 import org.koin.dsl.module
-import org.mockito.Mockito.`when`
 import org.mockito.Mockito.mock
+import androidx.fragment.app.testing.launchFragmentInContainer
+import androidx.test.espresso.action.ViewActions.click
+import kotlinx.coroutines.runBlocking
+import kotlinx.coroutines.withTimeout
+import org.mockito.Mockito.`when`
 
 @RunWith(AndroidJUnit4::class)
 @LargeTest
@@ -31,7 +31,6 @@ class AdviceFragmentTest {
             single {db}
         })
     )
-
     @Test
     fun textViewDisplaysStringFromListOfAdvices() {
         runBlocking {
@@ -41,13 +40,12 @@ class AdviceFragmentTest {
             launchFragmentInContainer<AdviceFragment>()
 
             withTimeout(5000) {
-                // get the advices retrieved from the database
-                // Find the TextView by its ID and check if it's displayed
-                onView(withId(R.id.adviceFragmentTextView)).check(matches(isDisplayed()))
-                // Check if the TextView has text that is contained in the stringList
-                onView(withId(R.id.adviceFragmentTextView)).check(matches(withText(
-                    hasStringFromCollection(advices)
-                )))
+                // get the advices retrieved from the database -> To be done next Sprint
+
+                onView(withId(R.id.fragmentContainerView)).check(matches(isDisplayed()))
+                // Find the expandable_lists by its ID and check if it's displayed
+                onView(withId(R.id.expandable_list1)).perform(click()).check(matches(isDisplayed()))
+
             }
         }
     }
