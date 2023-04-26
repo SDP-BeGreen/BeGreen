77c3f5d40fb5680c9542d76341599973a9f089eb
Xavier <xavier.ogay@epfl.ch>
2023-04-27 00:39:00 +0200

    Need help for last test

--- app/src/androidTest/java/com/github/sdp_begreen/begreen/fragments/CameraWithUIFragmentTest.kt
+++ app/src/androidTest/java/com/github/sdp_begreen/begreen/fragments/CameraWithUIFragmentTest.kt
@@ -4,6 +4,8 @@ import android.Manifest
 import android.graphics.Bitmap
 import android.os.Bundle
 import android.provider.MediaStore
+import androidx.fragment.app.add
+import androidx.fragment.app.commit
 import androidx.fragment.app.testing.FragmentScenario
 import androidx.fragment.app.testing.launchFragmentInContainer
 import androidx.fragment.app.viewModels
@@ -14,10 +16,13 @@ import androidx.test.espresso.assertion.ViewAssertions.matches
 import androidx.test.espresso.intent.Intents
 import androidx.test.espresso.intent.matcher.IntentMatchers
 import androidx.test.espresso.matcher.ViewMatchers
+import androidx.test.ext.junit.rules.ActivityScenarioRule
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import androidx.test.filters.LargeTest
 import androidx.test.rule.GrantPermissionRule
 import com.github.sdp_begreen.begreen.R
+import com.github.sdp_begreen.begreen.activities.MainActivity
+import com.github.sdp_begreen.begreen.activities.MainActivityTest
 import com.github.sdp_begreen.begreen.firebase.Auth
 import com.github.sdp_begreen.begreen.firebase.DB
 import com.github.sdp_begreen.begreen.models.User
@@ -31,6 +36,7 @@ import org.junit.BeforeClass
 import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
+import org.koin.android.scope.newScope
 import org.koin.dsl.module
 import org.mockito.Mockito
 
@@ -57,9 +63,14 @@ class CameraWithUIFragmentTest {
                 // user between tests, by simply pushing a new userId
                 Mockito.`when`(auth.getFlowUserIds())
                     .thenReturn(MutableStateFlow(user.id))
+                Mockito.`when`(auth.getConnectedUserId())
+                    .thenReturn(user.id)
             }
         }
     }
+
+    @get:Rule
+    val activityRule = ActivityScenarioRule(MainActivity::class.java)
 
     @get:Rule
     val koinTestRule = KoinTestRule(
@@ -86,7 +97,7 @@ class CameraWithUIFragmentTest {
     fun clickOnTakePhotoRedirectOnSharePost() {
         // Click the add new post button
         Espresso.onView(ViewMatchers.withId(R.id.camera_capture_button)).perform(ViewActions.click())
-        Thread.sleep(1000)
+        Thread.sleep(2000)
         Espresso.onView(ViewMatchers.withId(R.id.post_background)).check(matches(ViewMatchers.isDisplayed()))
     }
 
@@ -99,9 +110,9 @@ class CameraWithUIFragmentTest {
         }
         // Click the add new post button
         Espresso.onView(ViewMatchers.withId(R.id.camera_capture_button)).perform(ViewActions.click())
-        Thread.sleep(1000)
+        Thread.sleep(2000)
         Espresso.onView(ViewMatchers.withId(R.id.send_post)).perform(ViewActions.click())
-        Thread.sleep(1000)
+        Thread.sleep(2000)
         Espresso.onView(ViewMatchers.withId(R.id.camera_capture_button)).check(matches(ViewMatchers.isDisplayed()))
     }
 
@@ -109,7 +120,7 @@ class CameraWithUIFragmentTest {
     fun clickOnSwitchRedirectOnCamera() {
         // Click the add new post button
         Espresso.onView(ViewMatchers.withId(R.id.img_switch_camera)).perform(ViewActions.click())
-        Thread.sleep(1000)
+        Thread.sleep(2000)
         Espresso.onView(ViewMatchers.withId(R.id.camera_capture_button)).check(matches(ViewMatchers.isDisplayed()))
     }
 
@@ -123,13 +134,21 @@ class CameraWithUIFragmentTest {
         Espresso.onView(ViewMatchers.withId(R.id.userSearch)).check(matches(ViewMatchers.isDisplayed()))
     }
 
-
+//
     //@Test
     //fun clickOnProfileRedirectOnProfileDetails() {
     //    fragmentScenario.onFragment {
     //        val connectedUserViewModel
     //                by it.viewModels<ConnectedUserViewModel>(ownerProducer = { it.requireActivity() })
     //        connectedUserViewModel.setCurrentUser(user)
+    //        val fragmentManager = it.requireActivity().supportFragmentManager
+    //        val supportFragmentManager = it.childFragmentManager
+    //        supportFragmentManager.commit {
+    //            setReorderingAllowed(true)
+    //            add<CameraWithUIFragment>(R.id.mainFragmentContainer)
+    //        }
+    //        val fra = fragmentManager.findFragmentById(R.id.mainFragmentContainer)!!
+    //        fragmentManager.beginTransaction().add(fra, "mainFragmentContainer").commit()
     //    }
 //
     //    Espresso.onView(ViewMatchers.withId(R.id.profile_cam)).perform(ViewActions.click())
