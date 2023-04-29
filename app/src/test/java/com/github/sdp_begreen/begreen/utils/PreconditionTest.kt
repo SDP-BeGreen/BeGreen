f116d3ef7ebde4f4f985c8e2ffe6a7269845c0ca
David <dschroeter96@gmail.com>
2023-04-29 14:25:34 +0200

    test chackArgument does not throw if correct

--- app/src/test/java/com/github/sdp_begreen/begreen/utils/PreconditionTest.kt
+++ app/src/test/java/com/github/sdp_begreen/begreen/utils/PreconditionTest.kt
@@ -3,6 +3,7 @@ package com.github.sdp_begreen.begreen.utils
 import org.hamcrest.CoreMatchers.`is`
 import org.hamcrest.MatcherAssert.assertThat
 import org.junit.Assert.assertThrows
+import org.junit.Assert.fail
 import org.junit.Test
 
 class PreconditionTest {
@@ -14,5 +15,14 @@ class PreconditionTest {
         }
 
         assertThat(error.message, `is`("some message"))
+    }
+
+    @Test
+    fun checkArgumentDoNotThrowForCorrectArgument() {
+        try {
+            checkArgument(true, "Should not be thrown")
+        } catch (e: IllegalArgumentException) {
+            fail("Should not catch any exception")
+        }
     }
 }
