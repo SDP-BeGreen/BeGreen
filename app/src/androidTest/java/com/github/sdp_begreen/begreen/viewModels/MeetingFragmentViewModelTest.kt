e56a12b9d5fc8b9280725b3cccd9ece1fe4d76e7
David <dschroeter96@gmail.com>
2023-05-01 12:00:05 +0200

    implment test for MeetingFragmentViewModel

---
+++ app/src/androidTest/java/com/github/sdp_begreen/begreen/viewModels/MeetingFragmentViewModelTest.kt
@@ -0,0 +1,345 @@
+package com.github.sdp_begreen.begreen.viewModels
+
+import androidx.test.espresso.matcher.ViewMatchers.assertThat
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import androidx.test.filters.MediumTest
+import com.github.sdp_begreen.begreen.firebase.meetingServices.MeetingParticipantService
+import com.github.sdp_begreen.begreen.firebase.meetingServices.MeetingService
+import com.github.sdp_begreen.begreen.models.CustomLatLng
+import com.github.sdp_begreen.begreen.models.User
+import com.github.sdp_begreen.begreen.models.meetings.Meeting
+import com.github.sdp_begreen.begreen.rules.CoroutineTestRule
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.channels.Channel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.flowOf
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.test.runTest
+import org.hamcrest.Matchers.equalTo
+import org.hamcrest.Matchers.`is`
+import org.hamcrest.Matchers.nullValue
+import org.junit.Before
+import org.junit.BeforeClass
+import org.junit.Rule
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.koin.dsl.module
+import org.koin.test.KoinTestRule
+import org.mockito.Mockito.mock
+import org.mockito.Mockito.`when`
+
+@OptIn(ExperimentalCoroutinesApi::class)
+@RunWith(AndroidJUnit4::class)
+@MediumTest
+class MeetingFragmentViewModelTest {
+
+    companion object {
+        private val meetingService: MeetingService = mock(MeetingService::class.java)
+        private val participantService: MeetingParticipantService =
+            mock(MeetingParticipantService::class.java)
+
+        private val initiallyConnectedUser = User("123456", 10, "User 1")
+        private val currentUser: MutableStateFlow<User?> = MutableStateFlow(initiallyConnectedUser)
+
+        private val meetings = listOf(
+            Meeting(
+                "m1",
+                "John",
+                "Downtown Cleanup",
+                "Let's meet up and clean up the downtown area",
+                1718445600000,
+                1718467200000,
+                CustomLatLng(37.7749, -122.4194),
+                CustomLatLng(37.7749, -122.4194)
+            ),
+            Meeting(
+                "m2",
+                "Sarah",
+                "Park Cleanup",
+                "Let's meet up and clean up the local park",
+                1718186400000,
+                1718208000000,
+                CustomLatLng(37.7739, -122.4312),
+                CustomLatLng(37.7739, -122.4312)
+            ),
+            Meeting(
+                "m3",
+                "Tom",
+                "Beach Cleanup",
+                "Let's meet up and clean up the local beach",
+                1710237600000,
+                1710252000000,
+                CustomLatLng(46.517355, 6.628854),
+                CustomLatLng(46.517355, 6.628854)
+            ),
+        )
+
+        private val meetingsFlow = MutableStateFlow(meetings)
+
+        val addRemoveParticipantChannel = Channel<String>(1)
+
+        @BeforeClass
+        @JvmStatic
+        fun setUpAuth() {
+            runTest {
+
+                // Initial setup of getAllMeetings
+                `when`(meetingService.getAllMeetings()).thenReturn(meetingsFlow)
+
+                // setup getAllParticipants for initials meetings
+                `when`(participantService.getAllParticipants(meetings[0].meetingId!!)).thenReturn(
+                    flowOf(listOf("aaaaaa", "bbbbbb", "cccccc"))
+                )
+                `when`(participantService.getAllParticipants(meetings[1].meetingId!!)).thenReturn(
+                    flowOf(listOf("dddddd"))
+                )
+                `when`(participantService.getAllParticipants(meetings[2].meetingId!!)).thenReturn(
+                    flowOf(listOf("aaaaaa", "123456"))
+                )
+
+                `when`(
+                    participantService.addParticipant(
+                        meetings[1].meetingId!!,
+                        initiallyConnectedUser.id
+                    )
+                ).then {
+                    addRemoveParticipantChannel.trySend(it.arguments[1] as String)
+                }
+
+                `when`(
+                    participantService.removeParticipant(
+                        meetings[2].meetingId!!,
+                        initiallyConnectedUser.id
+                    )
+                ).then {
+                    addRemoveParticipantChannel.trySend(it.arguments[1] as String)
+                }
+            }
+        }
+    }
+
+    @get:Rule
+    val coroutineRules = CoroutineTestRule()
+
+    @get:Rule
+    val koinTestRule = KoinTestRule.create {
+        modules(module {
+            single { meetingService }
+            single { participantService }
+        })
+    }
+
+    @Before
+    fun setUpVM() {
+        // reset user meetings and user
+        meetingsFlow.tryEmit(meetings)
+        currentUser.tryEmit(initiallyConnectedUser)
+        vm = MeetingFragmentViewModel(currentUser)
+    }
+
+    private lateinit var vm: MeetingFragmentViewModel
+
+
+    @Test
+    fun allMeetingsReturnCorrectFlowOfMeetings() {
+        runTest {
+            val channel = Channel<List<Meeting>>(1)
+            backgroundScope.launch {
+                vm.allMeetings.collect {
+                    channel.send(it)
+                }
+            }
+
+            // assert correct meetings initially
+            assertThat(channel.receive(), `is`(equalTo(meetings)))
+
+            val newMeetings = listOf(
+                meetings[0],
+                meetings[1].copy(description = "Modified second meeting", title = "Clean School"),
+                meetings[2]
+            )
+            // modify meeting
+            meetingsFlow.emit(newMeetings)
+
+            assertThat(channel.receive(), `is`(equalTo(newMeetings)))
+        }
+    }
+
+    @Test
+    fun participationMapCorrectlyUpdateUponCurrentUserChange() {
+        runTest {
+            val channel = Channel<Map<String, Boolean>>(1)
+            backgroundScope.launch {
+                vm.participationMap.collect {
+                    channel.send(it)
+                }
+            }
+
+            // assert that initially should be
+            val initialMap = mapOf(
+                meetings[0].meetingId!! to false,
+                meetings[1].meetingId!! to false,
+                meetings[2].meetingId!! to true,
+            )
+            assertThat(channel.receive(), `is`(equalTo(initialMap)))
+
+            // modify connected user
+            currentUser.emit(User("aaaaaa", 12, "User 2"))
+            val map2 = mapOf(
+                meetings[0].meetingId!! to true,
+                meetings[1].meetingId!! to false,
+                meetings[2].meetingId!! to true,
+            )
+            assertThat(channel.receive(), `is`(equalTo(map2)))
+
+            // modify connected user
+            currentUser.emit(User("dddddd", 16, "User 3"))
+            val map3 = mapOf(
+                meetings[0].meetingId!! to false,
+                meetings[1].meetingId!! to true,
+                meetings[2].meetingId!! to false,
+            )
+            assertThat(channel.receive(), `is`(equalTo(map3)))
+        }
+    }
+
+    @Test
+    fun participationMapCorrectlyUpdateUponMeetingsChange() {
+        val newMeeting1 = meetings + Meeting(
+            "m4",
+            "Kim",
+            "River Cleanup",
+            "Let's meet up and clean up the local river",
+            1723464000000,
+            1723485600000,
+            CustomLatLng(46.519075, 6.561628),
+            CustomLatLng(46.519075, 6.561628)
+        )
+
+        val newMeeting2 = newMeeting1 - meetings[2]
+
+        val vm = MeetingFragmentViewModel(currentUser)
+
+        runTest {
+            // mock new meeting
+            backgroundScope.launch {
+                `when`(participantService.getAllParticipants(newMeeting1[3].meetingId!!)).thenReturn(
+                    flowOf(listOf("cccccc", "123456"))
+                )
+            }
+
+            val channel = Channel<Map<String, Boolean>>(1)
+            backgroundScope.launch {
+                vm.participationMap.collect {
+                    channel.send(it)
+                }
+            }
+
+            // assert that initially should be
+            val initialMap = mapOf(
+                meetings[0].meetingId!! to false,
+                meetings[1].meetingId!! to false,
+                meetings[2].meetingId!! to true,
+            )
+            assertThat(channel.receive(), `is`(equalTo(initialMap)))
+
+            meetingsFlow.emit(newMeeting1)
+            val map2 = mapOf(
+                newMeeting1[0].meetingId!! to false,
+                newMeeting1[1].meetingId!! to false,
+                newMeeting1[2].meetingId!! to true,
+                newMeeting1[3].meetingId!! to true,
+            )
+            assertThat(channel.receive(), `is`(equalTo(map2)))
+
+            meetingsFlow.emit(newMeeting2)
+            val map3 = mapOf(
+                newMeeting2[0].meetingId!! to false,
+                newMeeting2[1].meetingId!! to false,
+                newMeeting2[2].meetingId!! to true,
+            )
+            assertThat(channel.receive(), `is`(equalTo(map3)))
+
+        }
+    }
+
+    @Test
+    fun addParticipantCorrectlyAddParticipantAndUpdateMap() {
+        runTest {
+
+            val channel = Channel<Map<String, Boolean>>(1)
+
+            backgroundScope.launch {
+                vm.participationMap.collect {
+                    channel.send(it)
+                }
+            }
+
+            assertThat(
+                vm.participate(meetings[1].meetingId!!),
+                `is`(equalTo(meetings[1].meetingId!!))
+            )
+
+            assertThat(
+                addRemoveParticipantChannel.receive(),
+                `is`(equalTo(initiallyConnectedUser.id))
+            )
+
+            val mapAfterAddParticipation = mapOf(
+                meetings[0].meetingId!! to false,
+                meetings[1].meetingId!! to true,
+                meetings[2].meetingId!! to true,
+            )
+
+            assertThat(channel.receive(), `is`(equalTo(mapAfterAddParticipation)))
+        }
+    }
+
+    @Test
+    fun removeParticipantCorrectlyRemoveParticipantAndUpdateMap() {
+        runTest {
+
+            val channel = Channel<Map<String, Boolean>>(1)
+
+            backgroundScope.launch {
+                vm.participationMap.collect {
+                    channel.send(it)
+                }
+            }
+
+            assertThat(vm.withdraw(meetings[2].meetingId!!), `is`(equalTo(meetings[2].meetingId!!)))
+
+            assertThat(
+                addRemoveParticipantChannel.receive(),
+                `is`(equalTo(initiallyConnectedUser.id))
+            )
+
+            val mapAfterRemoveParticipation = mapOf(
+                meetings[0].meetingId!! to false,
+                meetings[1].meetingId!! to false,
+                meetings[2].meetingId!! to false,
+            )
+
+            assertThat(channel.receive(), `is`(equalTo(mapAfterRemoveParticipation)))
+        }
+    }
+
+    @Test
+    fun addParticipantNonConnectedUserShouldReturnNull() {
+        runTest {
+
+            currentUser.emit(null) // simulate no connected user
+            assertThat(vm.participate(meetings[1].meetingId!!), `is`(nullValue()))
+        }
+    }
+
+    @Test
+    fun removeParticipantNonConnectedUserShouldReturnNull() {
+        runTest {
+
+            currentUser.emit(null) // simulate no connected user
+            assertThat(vm.withdraw(meetings[1].meetingId!!), `is`(nullValue()))
+        }
+    }
+
+}
--- app/src/main/java/com/github/sdp_begreen/begreen/viewModels/MeetingFragmentViewModel.kt
+++ app/src/main/java/com/github/sdp_begreen/begreen/viewModels/MeetingFragmentViewModel.kt
@@ -4,77 +4,92 @@ import android.util.Log
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
 import androidx.lifecycle.viewModelScope
-import com.github.sdp_begreen.begreen.firebase.DB
-import com.github.sdp_begreen.begreen.models.CustomLatLng
+import com.github.sdp_begreen.begreen.firebase.meetingServices.MeetingParticipantService
+import com.github.sdp_begreen.begreen.firebase.meetingServices.MeetingService
 import com.github.sdp_begreen.begreen.models.User
-import com.github.sdp_begreen.begreen.models.meetings.Meeting
-import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.SharingStarted
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.combine
 import kotlinx.coroutines.flow.first
 import kotlinx.coroutines.flow.flow
-import kotlinx.coroutines.flow.flowOf
+import kotlinx.coroutines.flow.mapNotNull
 import kotlinx.coroutines.flow.stateIn
 import kotlinx.coroutines.launch
 import org.koin.java.KoinJavaComponent.inject
 
-class MeetingFragmentViewModel(currentUser: StateFlow<User?>) : ViewModel() {
+class MeetingFragmentViewModel(private val currentUser: StateFlow<User?>) : ViewModel() {
 
-    private val db by inject<DB>(DB::class.java) // TODO call the correct database
+    private val meetingService by inject<MeetingService>(MeetingService::class.java)
+    private val participantService by inject<MeetingParticipantService>(MeetingParticipantService::class.java)
 
-    private val mutableParticipationMap: MutableStateFlow<Map<String, Boolean>> = MutableStateFlow(emptyMap())
+    private val mutableParticipationMap: MutableStateFlow<Map<String, Boolean>> =
+        MutableStateFlow(emptyMap())
 
     val participationMap = mutableParticipationMap.asStateFlow()
     val allMeetings = flow {
-        getAllMeetings().collect {
+        meetingService.getAllMeetings().collect {
             emit(it)
         }
     }.stateIn(viewModelScope, SharingStarted.Eagerly, emptyList())
-
-    // TODO will come from db
-    private suspend fun getAllParticipants(meetingId: String): Flow<List<String>> = flowOf(
-        emptyList()
-    )
-
-
-    // TODO this function will be replace by actual db call
-    private suspend fun getAllMeetings(): Flow<List<Meeting>> = flowOf(tempMeetings)
 
     init {
         // Dynamically generate the map that contains to which meeting we are subscribed
         // will emit a new value if the the user changes, or if the list of meeting changes
         viewModelScope.launch {
-            currentUser.collect { cUser ->
-                cUser?.also { user ->
-                    allMeetings.collect { meetings ->
-                        Log.d("Print test participation view model", "Passe in init")
-                        mutableParticipationMap.emit(
-                            meetings.mapNotNull { meeting ->
-                                meeting.meetingId?.let { id ->
-                                    id to getAllParticipants(id).first()
-                                }
-                            }.associate {
-                                it.first to it.second.contains(user.id)
-                            }
-                        )
+            // subscribe to user connection changes
+            currentUser.mapNotNull { it?.id }.combine(allMeetings) { userId, meetings ->
+                meetings.mapNotNull { meeting ->
+                    meeting.meetingId?.let { id ->
+                        id to participantService.getAllParticipants(
+                            id
+                        ).first()
                     }
+                }.associate {
+                    it.first to it.second.contains(userId)
                 }
-            }
+            }.collect { mutableParticipationMap.emit(it) }
         }
     }
 
-    fun participate(meetingId: String) {
-        Log.d("Print test participation view model", "participate: $meetingId")
-        // TODO do the actual call to the database as well
-        mutableParticipationMap.tryEmit(mutableParticipationMap.value + (meetingId to true))
+    /**
+     * Function to participate to a meeting
+     *
+     * @param meetingId The meeting to which to participate to
+     *
+     * @return The meeting id if it was possible to participate to it (i.e. a user was logged in)
+     */
+    fun participate(meetingId: String): String? {
+        currentUser.value?.also {
+            viewModelScope.launch {
+                Log.d("Print test participation view model", "participate: ${it.id}")
+                participantService.addParticipant(meetingId, it.id)
+            }
+            mutableParticipationMap.tryEmit(mutableParticipationMap.value + (meetingId to true))
+            return meetingId
+        }
+        Log.d("Meeting Fragment View Model", "Error while adding participant (No connectedUser)")
+        return null
     }
 
-    fun withdraw(meetingId: String) {
-        Log.d("Print test participation view model", "withdraw: $meetingId")
-        // TODO do the actual call to the database as well
-        mutableParticipationMap.tryEmit(mutableParticipationMap.value - meetingId)
+    /**
+     * Function to withdraw to a meeting
+     *
+     * @param meetingId The meeting to which to withdraw from
+     *
+     * @return The meeting id if it was possible to withdraw from it (i.e. a user was logged in)
+     */
+    fun withdraw(meetingId: String): String? {
+        currentUser.value?.also {
+            viewModelScope.launch {
+                participantService.removeParticipant(meetingId, it.id)
+            }
+            mutableParticipationMap.tryEmit(mutableParticipationMap.value + (meetingId to false))
+            return meetingId
+        }
+        Log.d("Meeting Fragment View Model", "Error while removing participant (No connectedUser)")
+        return null
     }
 
     companion object {
@@ -89,6 +104,8 @@ class MeetingFragmentViewModel(currentUser: StateFlow<User?>) : ViewModel() {
 
 }
 
+//TODO keep here for not, will be used to populate db
+/*
 private val tempMeetings = listOf(
     Meeting(
         "m1",
@@ -241,3 +258,4 @@ private val tempMeetings = listOf(
         CustomLatLng(46.806832, 7.156354)
     )
 )
+*/
