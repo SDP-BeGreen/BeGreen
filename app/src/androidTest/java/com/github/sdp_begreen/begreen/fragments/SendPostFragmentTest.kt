5e80f59e6a89c56889e51a9ad39697557612931c
Xavier <xavier.ogay@epfl.ch>
2023-05-01 13:46:15 +0200

    Add tests for SendPostFragment

--- app/src/androidTest/java/com/github/sdp_begreen/begreen/fragments/SendPostFragmentTest.kt
+++ app/src/androidTest/java/com/github/sdp_begreen/begreen/fragments/SendPostFragmentTest.kt
@@ -5,9 +5,15 @@ import android.os.Bundle
 import androidx.fragment.app.testing.FragmentScenario
 import androidx.fragment.app.testing.launchFragmentInContainer
 import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.ViewAction
 import androidx.test.espresso.action.ViewActions
+import androidx.test.espresso.action.ViewActions.click
+import androidx.test.espresso.action.ViewActions.typeText
 import androidx.test.espresso.assertion.ViewAssertions.matches
 import androidx.test.espresso.matcher.ViewMatchers
+import androidx.test.espresso.matcher.ViewMatchers.isRoot
+import androidx.test.espresso.matcher.ViewMatchers.withId
+import androidx.test.espresso.matcher.ViewMatchers.withText
 import androidx.test.ext.junit.rules.ActivityScenarioRule
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import androidx.test.filters.LargeTest
@@ -99,21 +105,36 @@ class SendPostFragmentTest {
     }
 
     @Test
-    fun clickCategoryWorks() {
-        // Click the category input
-        onView(ViewMatchers.withId(R.id.post_category)).perform(ViewActions.click()).check(
+    fun categoryIsDisplayed() {
+        // Check if the category input is displayed
+        onView(withId(R.id.post_category)).check(
             matches(
                 ViewMatchers.isDisplayed()
             )
         )
+    }
+
+    @Test
+    fun DescriptionIsDisplayed() {
+        // Check if the description input is displayed
+        onView(withId(R.id.post_description)).check(
+            matches(
+                ViewMatchers.isDisplayed()
+            )
+        )
+    }
+
+    @Test
+    fun clickCategoryWorks() {
+        // Check the category input
+        onView(withId(R.id.post_category)).perform(typeText("test"))
+        onView(withId(R.id.post_category)).check(matches(withText("test")))
     }
     @Test
     fun clickDescriptionWorks() {
-        // Click the description input
-        onView(ViewMatchers.withId(R.id.post_category)).perform(ViewActions.click()).check(
-            matches(
-                ViewMatchers.isDisplayed()
-            )
-        )
+        // Check the description input
+        onView(withId(R.id.post_description)).perform(typeText("test"))
+        onView(withId(R.id.post_description)).check(matches(withText("test")))
     }
+
 }
