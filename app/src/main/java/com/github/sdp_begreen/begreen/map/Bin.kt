788c5b8d4ec84d4d4958276598c8f94df84c8384
merigh <ilias.merigh@epfl.ch>
2023-05-04 14:03:23 +0200

    Change Bin type equals

--- app/src/main/java/com/github/sdp_begreen/begreen/map/Bin.kt
+++ app/src/main/java/com/github/sdp_begreen/begreen/map/Bin.kt
@@ -2,7 +2,6 @@ package com.github.sdp_begreen.begreen.map
 
 import com.github.sdp_begreen.begreen.models.CustomLatLng
 import com.github.sdp_begreen.begreen.models.TrashCategory
-import com.github.sdp_begreen.begreen.models.TrashPhotoMetadata
 
 data class Bin(var id: String? = null, val type: TrashCategory, val location : CustomLatLng) {
 
@@ -18,18 +17,4 @@ data class Bin(var id: String? = null, val type: TrashCategory, val location : C
         type,
         CustomLatLng(location.latitude, location.longitude)
     )
-
-    override fun equals(other: Any?): Boolean {
-        if (this === other) return true
-        if (other !is Bin) return false
-
-        // ProfilePhotoMetadata is uniquely defined by the picture id because if we compare all fields, some of them
-        // are obsolete in the database. Actually this was the case in our case.
-
-        return id == other.id
-    }
-
-    override fun hashCode(): Int {
-        return id?.hashCode() ?: 0
-    }
 }
