82a45ce2fd68a130274634e0a765d5b8ee075952
David <dschroeter96@gmail.com>
2023-05-11 09:28:12 +0200

    try to increase ContestParticipant coverage with a few more tests

--- app/src/test/java/com/github/sdp_begreen/begreen/models/event/ContestParticipantTest.kt
+++ app/src/test/java/com/github/sdp_begreen/begreen/models/event/ContestParticipantTest.kt
@@ -1,5 +1,6 @@
 package com.github.sdp_begreen.begreen.models.event
 
+import org.hamcrest.CoreMatchers.equalTo
 import org.hamcrest.CoreMatchers.`is`
 import org.hamcrest.MatcherAssert.assertThat
 import org.junit.Test
@@ -7,7 +8,23 @@ import org.junit.Test
 class ContestParticipantTest {
     @Test
     fun contestParticipantToStringReturnExpectedString() {
-        val newContest = ContestParticipant("1", 156)
-        assertThat(newContest.toString(), `is`("Participant: 1, score: 156"))
+        val contestParticipant = ContestParticipant("1", 156)
+        assertThat(contestParticipant.toString(), `is`("Participant: 1, score: 156"))
+    }
+
+    @Test
+    fun modifyContestParticipantIdCorrectlyModifiesIt() {
+        val contestParticipant = ContestParticipant("1")
+        assertThat(contestParticipant.id, `is`(equalTo("1")))
+        contestParticipant.id = "newId"
+        assertThat(contestParticipant.id, `is`(equalTo("newId")))
+    }
+
+    @Test
+    fun modifyContestParticipantScoreCorrectlyModifiesIt() {
+        val contestParticipant = ContestParticipant("1", 123)
+        assertThat(contestParticipant.score, `is`(equalTo(123)))
+        contestParticipant.score = 457
+        assertThat(contestParticipant.score, `is`(equalTo(457)))
     }
 }
