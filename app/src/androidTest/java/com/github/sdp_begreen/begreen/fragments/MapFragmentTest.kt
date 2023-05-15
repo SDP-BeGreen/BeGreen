4b675a9f85c97a50b009041c89ecd9699d61588c
Luca <luca.carroz@epfl.ch>
2023-05-15 17:15:18 +0200

    Added and improved tests for the Map fragment

--- app/src/androidTest/java/com/github/sdp_begreen/begreen/fragments/MapFragmentTest.kt
+++ app/src/androidTest/java/com/github/sdp_begreen/begreen/fragments/MapFragmentTest.kt
@@ -1,8 +1,12 @@
 package com.github.sdp_begreen.begreen.fragments
 
 import android.Manifest
+import android.content.Context
+import androidx.fragment.app.testing.FragmentScenario
 import androidx.fragment.app.testing.launchFragmentInContainer
+import androidx.test.core.app.ApplicationProvider
 import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.action.ViewActions.click
 import androidx.test.espresso.assertion.ViewAssertions.matches
 import androidx.test.espresso.matcher.ViewMatchers.*
 import androidx.test.ext.junit.runners.AndroidJUnit4
@@ -11,21 +15,22 @@ import androidx.test.rule.GrantPermissionRule
 import com.github.sdp_begreen.begreen.R
 import com.github.sdp_begreen.begreen.firebase.DB
 import com.github.sdp_begreen.begreen.map.Bin
-import com.github.sdp_begreen.begreen.models.CustomLatLng
 import com.github.sdp_begreen.begreen.models.TrashCategory
 import com.github.sdp_begreen.begreen.rules.KoinTestRule
-import com.google.android.gms.maps.model.LatLng
-import kotlinx.coroutines.runBlocking
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.test.runTest
 import org.hamcrest.Matchers.*
+import org.junit.Before
+import org.junit.BeforeClass
 import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
 import org.koin.dsl.module
-import org.mockito.ArgumentMatchers.*
-import org.mockito.Mockito
+import org.mockito.Mockito.mock
+import org.mockito.kotlin.*
 
 
-//@OptIn(ExperimentalCoroutinesApi::class)
+@OptIn(ExperimentalCoroutinesApi::class)
 @RunWith(AndroidJUnit4::class)
 @LargeTest
 class MapFragmentTest {
@@ -35,96 +40,81 @@ class MapFragmentTest {
      */
     companion object {
 
-        private val db: DB = Mockito.mock(DB::class.java)
+        private val db: DB = mock(DB::class.java)
 
         private val bins = mutableListOf(
             Bin("1", TrashCategory.CLOTHES, 4.3, 2.8),
-            Bin("2", TrashCategory.PAPER,56.3, 22.3),
-            Bin("3", TrashCategory.CLOTHES,6.0, 9.0)
+            Bin("2", TrashCategory.PAPER, 56.3, 22.3),
+            Bin("3", TrashCategory.CLOTHES, 6.0, 9.0)
         )
 
-
-        /*
         @BeforeClass
         @JvmStatic
         fun setUp() {
             runTest {
 
-                Mockito.`when`(db.removeBin(anyString())).then {
+                whenever(db.removeBin(any())).then {
                     bins.filter { bin -> bin.id?.equals(it.arguments[0])?.not() ?: true }
                 }
-                Mockito.`when`(db.addBin(any(Bin::class.java) )).thenReturn(true)
-                Mockito.`when`(db.getAllBins()).thenReturn(bins)
+                whenever(db.addBin(any())).then {
+                    bins.add(it.arguments[0] as Bin)
+                    true
+                }
+                whenever(db.getAllBins()).thenReturn(bins)
             }
-        } Doesnt work, for strange reasons */
+        }
+    }
+
+    private lateinit var fragmentScenario: FragmentScenario<MapFragment>
+
+    @Before
+    fun setup() {
+        fragmentScenario = launchFragmentInContainer()
     }
 
     @get:Rule
     val koinTestRule = KoinTestRule(
         modules = listOf(module {
-            single {db}
+            single { db }
         })
     )
 
     @get:Rule
-    val fineLocationPermissionRule: GrantPermissionRule = GrantPermissionRule.grant(Manifest.permission.ACCESS_FINE_LOCATION)
+    val fineLocationPermissionRule: GrantPermissionRule =
+        GrantPermissionRule.grant(Manifest.permission.ACCESS_FINE_LOCATION)
 
     @get:Rule
-    val coarseLocationPermissionRule: GrantPermissionRule = GrantPermissionRule.grant(Manifest.permission.ACCESS_COARSE_LOCATION)
-
-    private val fragment = MapFragment()
-
+    val coarseLocationPermissionRule: GrantPermissionRule =
+        GrantPermissionRule.grant(Manifest.permission.ACCESS_COARSE_LOCATION)
 
     @Test
-    fun testFragmentInflation() {
-
-        /* The googlemaps library is quite hard to test. For now, we have only tested the launching of the mapFragment.
-
-        According to the bootcamp https://github.com/sweng-epfl/public/blob/main/project/bootcamp/Maps.md :
-
-        [...] since you cannot set the inputs, it will be difficult to achieve 100% of coverage.
-        Therefore, we advise you to write as much as possible code that is independent from map components, and that you can easily test.
-        */
-
-
-        runBlocking {
-
-            Mockito.`when`(db.getAllBins()).thenReturn(bins)
-
-            launchFragmentInContainer { fragment }
-
-            // Wait until the map fragment is displayed
-            onView(withId(R.id.mapFragment)).check(matches(isDisplayed()))
-        }
+    fun mapIsDisplayed() {
+        onView(withId(R.id.mapFragment)).check(matches(isDisplayed()))
     }
 
-    /*
+    @Test
+    fun addBinButtonIsDisplayedWithCorrectText() {
 
-    This test doesn't pas CI tests for now but only local tests
+        val expectedText =
+            ApplicationProvider.getApplicationContext<Context>().getString(R.string.add_new_bin)
+        onView(withId(R.id.binBtn)).check(matches(isDisplayed()))
+        onView(withId(R.id.binBtn)).check(matches(withText(expectedText)))
+    }
+
+    @Test
+    fun trashCategorySelectorIsDisplayed() {
+        onView(withId(R.id.trashCategorySelector)).check(matches(isDisplayed()))
+    }
 
     @Test
     fun clickOnAddNewBinBtnAddsNewBin() {
 
-        val nbOfBinsOld = BinsFakeDatabase.fakeBins.size
+        val initialNumberOfBins = bins.size
 
-        launchFragmentInContainer { fragment }
-
-        // Wait until the map fragment is displayed
-        onView(withId(R.id.mapFragment)).check(matches(isDisplayed()))
-
-        onView(withId(R.id.addNewBinBtn)).check(matches(isDisplayed()))
-
-        // Click on the addNewBinBtn
-        onView(withId(R.id.addNewBinBtn)).perform(ViewActions.click())
-
-        val nbOfBinsNew = BinsFakeDatabase.fakeBins.size
+        // Add a new bin by clicking the button
+        onView(withId(R.id.binBtn)).perform(click())
 
         // Check that a new bin have been added
-        assertThat(nbOfBinsNew, greaterThan(nbOfBinsOld))
+        assertThat(bins.size, `is`(equalTo(initialNumberOfBins + 1)))
     }
-
-    */
-
-    // After some researches, it seems that detecting a click on a googlemaps marker is not possible
-    // in a clean way and is quite difficult. Therefore, the remove bin cannot be tested.
 }
