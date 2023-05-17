ff15cd2f2a4be627c90ccea835ebe2353d9f45b9
Luca <luca.carroz@epfl.ch>
2023-05-17 20:18:11 +0200

    Deleted unused classes

--- app/src/main/java/com/github/sdp_begreen/begreen/map/location/LocationListener.kt
+++
@@ -1,7 +0,0 @@
-package com.github.sdp_begreen.begreen.map.location
-
-import com.google.android.gms.location.LocationResult
-
-interface LocationListener {
-    fun locationResponse(locationResult: LocationResult)
-}
