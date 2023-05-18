bee3f8f8d0e5e12bab8774b743a69b5c127f0f0e
2Tricyk4u <xavier.ogay@epfl.ch>
2023-05-18 15:00:54 +0200

    Reorganize file

--- app/src/main/java/com/github/sdp_begreen/begreen/fragments/ContestCreationFragment.kt
+++ app/src/main/java/com/github/sdp_begreen/begreen/fragments/ContestCreationFragment.kt
@@ -75,43 +75,68 @@ class ContestCreationFragment : Fragment() {
         return view
     }
 
+    //------------------------------------Title/Private--------------------------------------------
+
     /**
-     * Setup the confirm creation button
+     * Setup the title selection input
      * @param view the view of the fragment
      */
-    private fun setupConfirmCreationButton(view: View) {
-        val confirmCreationButton = view.findViewById<Button>(R.id.contest_confirm_button)
-        confirmCreationButton.setOnClickListener {
-            if (contestCreationViewModel.isContestCreationValid()) {
-                //TODO: Call firebase to create the contest
-                lifecycleScope.launch {
-                    requireActivity().supportFragmentManager.commit {
-                        setReorderingAllowed(true)
-                        replace(R.id.mainFragmentContainer, ContestsFragment())
-                    }
-                }
-            } else {
-                Toast.makeText(
-                    context,
-                    "Please fill all the fields with correct values",
-                    Toast.LENGTH_SHORT
-                ).show()
+    private fun setupTitle(view: View) {
+        val contestTitle = view.findViewById<TextInputEditText>(R.id.contest_creation_title)
+        contestTitle.setText(contestCreationViewModel.contestTitle)
+        contestTitle.setOnFocusChangeListener { v, hasFocus ->
+            if (!hasFocus) {
+                contestCreationViewModel.contestTitle = contestTitle.text.toString()
             }
         }
     }
 
+
     /**
-     * Setup the cancel creation button
+     * Setup the private checkbox
      * @param view the view of the fragment
      */
-    private fun setupCancelCreationButton(view: View) {
-        val cancelCreationButton = view.findViewById<Button>(R.id.contest_cancel_button)
-        cancelCreationButton.setOnClickListener {
-            lifecycleScope.launch {
-                requireActivity().supportFragmentManager.commit {
-                    setReorderingAllowed(true)
-                    replace(R.id.mainFragmentContainer, ContestsFragment())
-                }
+    private fun setupPrivateCheckbox(view: View) {
+        val privateCheckbox = view.findViewById<CheckBox>(R.id.private_contest_checkbox)
+        privateCheckbox.isChecked = contestCreationViewModel.isPrivate
+        privateCheckbox.setOnCheckedChangeListener { buttonView, isChecked ->
+            contestCreationViewModel.isPrivate = isChecked
+        }
+    }
+
+
+    //------------------------------------Location--------------------------------------------
+
+    /**
+     * Setup the Map image button
+     * @param view the view of the fragment
+     */
+    private fun setupMapButton(view: View) {
+        val mapButton = view.findViewById<ImageView>(R.id.contest_creation_location_map)
+        mapButton.setOnClickListener {
+
+        }
+    }
+
+    /**
+     * Setup the expand button for the location details
+     * @param view the view of the fragment
+     */
+    private fun setupExpandButton(view: View) {
+        val expandButton = view.findViewById<ImageView>(R.id.contest_creation_location_expand)
+        val locationDetailsContainer =
+            view.findViewById<View>(R.id.contest_location_details_container)
+
+        locationDetailsContainer.visibility = View.GONE
+        expandButton.setImageResource(R.drawable.ic_chevron_up)
+
+        expandButton.setOnClickListener {
+            if (locationDetailsContainer.visibility == View.GONE) {
+                locationDetailsContainer.visibility = View.VISIBLE
+                expandButton.setImageResource(R.drawable.ic_chevron_down)
+            } else {
+                locationDetailsContainer.visibility = View.GONE
+                expandButton.setImageResource(R.drawable.ic_chevron_up)
             }
         }
     }
@@ -136,6 +161,7 @@ class ContestCreationFragment : Fragment() {
         }
     }
 
+
     /**
      * Setup the postal code input of the contest
      * @param view the view of the fragment
@@ -152,26 +178,6 @@ class ContestCreationFragment : Fragment() {
         lifecycleScope.launch {
             contestCreationViewModel.postalCode.flowWithLifecycle(lifecycle).collect {
                 postalCodeText.setText(it)
-            }
-        }
-    }
-
-    /**
-     * Setup the radius of the contest input
-     * @param view the view of the fragment
-     */
-    private fun setupRadius(view: View) {
-        val radiusText = view.findViewById<TextInputEditText>(R.id.radius_contest_creation)
-        radiusText.setOnFocusChangeListener { v, hasFocus ->
-            if (!hasFocus) {
-                if (contestCreationViewModel.editRadius(Integer.parseInt(radiusText.text.toString()))) {
-                    radiusText.setText(contestCreationViewModel.radius.value.toString())
-                }
-            }
-        }
-        lifecycleScope.launch {
-            contestCreationViewModel.radius.flowWithLifecycle(lifecycle).collect {
-                radiusText.setText(it.toString() ?: "0")
             }
         }
     }
@@ -202,48 +208,57 @@ class ContestCreationFragment : Fragment() {
     }
 
     /**
-     * Setup the title selection input
+     * Setup the radius of the contest input
      * @param view the view of the fragment
      */
-    private fun setupTitle(view: View) {
-        val contestTitle = view.findViewById<TextInputEditText>(R.id.contest_creation_title)
-        contestTitle.setText(contestCreationViewModel.contestTitle)
-        contestTitle.setOnFocusChangeListener { v, hasFocus ->
+    private fun setupRadius(view: View) {
+        val radiusText = view.findViewById<TextInputEditText>(R.id.radius_contest_creation)
+        radiusText.setOnFocusChangeListener { v, hasFocus ->
             if (!hasFocus) {
-                contestCreationViewModel.contestTitle = contestTitle.text.toString()
+                if (contestCreationViewModel.editRadius(Integer.parseInt(radiusText.text.toString()))) {
+                    radiusText.setText(contestCreationViewModel.radius.value.toString())
+                }
+            }
+        }
+        lifecycleScope.launch {
+            contestCreationViewModel.radius.flowWithLifecycle(lifecycle).collect {
+                radiusText.setText(it.toString() ?: "0")
             }
         }
     }
 
+    //------------------------------------Timezone/Date--------------------------------------------
+
     /**
-     * Setup the private checkbox
+     * Populate the time zone spinner with the available time zones and set it to the default
      * @param view the view of the fragment
      */
-    private fun setupPrivateCheckbox(view: View) {
-        val privateCheckbox = view.findViewById<CheckBox>(R.id.private_contest_checkbox)
-        privateCheckbox.isChecked = contestCreationViewModel.isPrivate
-        privateCheckbox.setOnCheckedChangeListener { buttonView, isChecked ->
-            contestCreationViewModel.isPrivate = isChecked
+    private fun populateAndUpdateTimeZone(view: View) {
+        val spinner = view.findViewById<Spinner>(R.id.contest_timezone_spinner)
+        val idArray = TimeZone.getAvailableIDs()
+        val idAdapter = ArrayAdapter<String>(
+            requireContext(), android.R.layout.simple_spinner_dropdown_item,
+            idArray
+        )
+        idAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner.adapter = idAdapter;
+
+        // now set the spinner to default timezone from the time zone settings
+        for (i in idArray.indices) {
+            if (idArray[i] == TimeZone.getDefault().id) {
+                spinner.setSelection(i)
+                break
+            }
         }
     }
 
-    /**
-     * Setup the Map image button
-     * @param view the view of the fragment
-     */
-    private fun setupMapButton(view: View) {
-        val mapButton = view.findViewById<ImageView>(R.id.contest_creation_location_map)
-        mapButton.setOnClickListener {
-
-        }
-    }
 
     /**
      * Convert a formatted date to a timestamp
      */
     private fun fromFormattedDateToLong(date: String): Long {
         val formatter = SimpleDateFormat("dd/MM/yyyy")
-        if(date.contains(Regex("[a-zA-Z]"))) return 0
+        if (date.contains(Regex("[a-zA-Z]"))) return 0
         val datetmp = formatter.parse(date)
         return datetmp?.time ?: 0
     }
@@ -259,7 +274,11 @@ class ContestCreationFragment : Fragment() {
     /**
      * Add a listener to the date edit text
      */
-    private fun addDateListener(text: EditText, viewModelTimeValue: StateFlow<Long?>, editFun : (input: Long) -> Unit) {
+    private fun addDateListener(
+        text: EditText,
+        viewModelTimeValue: StateFlow<Long?>,
+        editFun: (input: Long) -> Unit
+    ) {
         text.setOnFocusChangeListener { v, hasFocus ->
             if (!hasFocus) {
                 val timestamp = fromFormattedDateToLong(text.text.toString())
@@ -280,8 +299,16 @@ class ContestCreationFragment : Fragment() {
         val startDateText = view.findViewById<EditText>(R.id.start_date_contest_text)
         val endDateText = view.findViewById<EditText>(R.id.end_date_contest_text)
 
-        addDateListener(startDateText, contestCreationViewModel.startDate, contestCreationViewModel::editStartDate)
-        addDateListener(endDateText, contestCreationViewModel.endDate, contestCreationViewModel::editEndDate)
+        addDateListener(
+            startDateText,
+            contestCreationViewModel.startDate,
+            contestCreationViewModel::editStartDate
+        )
+        addDateListener(
+            endDateText,
+            contestCreationViewModel.endDate,
+            contestCreationViewModel::editEndDate
+        )
     }
 
     /**
@@ -289,11 +316,11 @@ class ContestCreationFragment : Fragment() {
      * @return the new date picker
      */
     private fun getNewDatePicker(): MaterialDatePicker<Pair<Long, Long>> {
-        if(contestCreationViewModel.startDate.value == null || contestCreationViewModel.endDate.value == null) {
+        if (contestCreationViewModel.startDate.value == null || contestCreationViewModel.endDate.value == null) {
             contestCreationViewModel.editStartDate(System.currentTimeMillis())
             contestCreationViewModel.editEndDate(System.currentTimeMillis())
         }
-        if(contestCreationViewModel.startDate.value!! > contestCreationViewModel.endDate.value!!) {
+        if (contestCreationViewModel.startDate.value!! > contestCreationViewModel.endDate.value!!) {
             contestCreationViewModel.editStartDate(contestCreationViewModel.endDate.value!!)
         }
         val builder = MaterialDatePicker.Builder.dateRangePicker()
@@ -330,7 +357,7 @@ class ContestCreationFragment : Fragment() {
      */
     private fun setDateButtonNewListener(button: Button) {
         val datePicker = getNewDatePicker()
-       addListenerOnDatePicker(datePicker)
+        addListenerOnDatePicker(datePicker)
 
         button.setOnClickListener {
             datePicker.show(requireActivity().supportFragmentManager, "datePicker")
@@ -352,22 +379,24 @@ class ContestCreationFragment : Fragment() {
 
 
         lifecycleScope.launch {
-            contestCreationViewModel.startDate.flowWithLifecycle(lifecycle).combine(contestCreationViewModel.endDate){
-                startDate, endDate ->
-                if(startDate != null && endDate != null) {
-                    startDateText.setText(formatter.format(startDate))
-                    endDateText.setText(formatter.format(endDate))
-                    setDateButtonNewListener(startDateButton)
+            contestCreationViewModel.startDate.flowWithLifecycle(lifecycle)
+                .combine(contestCreationViewModel.endDate) { startDate, endDate ->
+                    if (startDate != null && endDate != null) {
+                        startDateText.setText(formatter.format(startDate))
+                        endDateText.setText(formatter.format(endDate))
+                        setDateButtonNewListener(startDateButton)
+                    }
                 }
-            }
         }
 
         startDateButton.setOnClickListener {
             datePicker.show(requireActivity().supportFragmentManager, "datePicker")
         }
 
-       addListenerOnDatePicker(datePicker)
+        addListenerOnDatePicker(datePicker)
     }
+
+    //----------------------------------Hours--------------------------------------------
 
     /**
      * Setup the start hour button
@@ -420,51 +449,49 @@ class ContestCreationFragment : Fragment() {
         }
     }
 
+    //------------------------------------Cancel/Confirm button------------------------------------
+
     /**
-     * Setup the expand button for the location details
+     * Setup the confirm creation button
      * @param view the view of the fragment
      */
-    private fun setupExpandButton(view: View) {
-        val expandButton = view.findViewById<ImageView>(R.id.contest_creation_location_expand)
-        val locationDetailsContainer =
-            view.findViewById<View>(R.id.contest_location_details_container)
-
-        locationDetailsContainer.visibility = View.GONE
-        expandButton.setImageResource(R.drawable.ic_chevron_up)
-
-        expandButton.setOnClickListener {
-            if (locationDetailsContainer.visibility == View.GONE) {
-                locationDetailsContainer.visibility = View.VISIBLE
-                expandButton.setImageResource(R.drawable.ic_chevron_down)
+    private fun setupConfirmCreationButton(view: View) {
+        val confirmCreationButton = view.findViewById<Button>(R.id.contest_confirm_button)
+        confirmCreationButton.setOnClickListener {
+            if (contestCreationViewModel.isContestCreationValid()) {
+                //TODO: Call firebase to create the contest
+                lifecycleScope.launch {
+                    requireActivity().supportFragmentManager.commit {
+                        setReorderingAllowed(true)
+                        replace(R.id.mainFragmentContainer, ContestsFragment())
+                    }
+                }
             } else {
-                locationDetailsContainer.visibility = View.GONE
-                expandButton.setImageResource(R.drawable.ic_chevron_up)
+                Toast.makeText(
+                    context,
+                    "Please fill all the fields with correct values",
+                    Toast.LENGTH_SHORT
+                ).show()
             }
         }
     }
 
     /**
-     * Populate the time zone spinner with the available time zones and set it to the default
+     * Setup the cancel creation button
      * @param view the view of the fragment
      */
-    private fun populateAndUpdateTimeZone(view: View) {
-        val spinner = view.findViewById<Spinner>(R.id.contest_timezone_spinner)
-        val idArray = TimeZone.getAvailableIDs()
-        val idAdapter = ArrayAdapter<String>(
-            requireContext(), android.R.layout.simple_spinner_dropdown_item,
-            idArray
-        )
-        idAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-        spinner.adapter = idAdapter;
-
-        // now set the spinner to default timezone from the time zone settings
-        for (i in idArray.indices) {
-            if (idArray[i] == TimeZone.getDefault().id) {
-                spinner.setSelection(i)
-                break
+    private fun setupCancelCreationButton(view: View) {
+        val cancelCreationButton = view.findViewById<Button>(R.id.contest_cancel_button)
+        cancelCreationButton.setOnClickListener {
+            lifecycleScope.launch {
+                requireActivity().supportFragmentManager.commit {
+                    setReorderingAllowed(true)
+                    replace(R.id.mainFragmentContainer, ContestsFragment())
+                }
             }
         }
     }
+
 
     /**
      * Companion object to create the fragment
