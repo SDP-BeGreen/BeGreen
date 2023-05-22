b7f494d5f6418d03924cf43da5f945d15ade71d0
David Schroeter <56824395+david-schroeter@users.noreply.github.com>
2023-05-22 20:24:33 +0200

    117 Revert map tests as it breaks CI (#118)
    * revert CI script with old device, and comment non-passing tests
    
    * commente more tests that rely on the map

--- app/src/androidTest/java/com/github/sdp_begreen/begreen/dialog/ContestMapDialogTest.kt
+++ app/src/androidTest/java/com/github/sdp_begreen/begreen/dialog/ContestMapDialogTest.kt
@@ -3,12 +3,10 @@ package com.github.sdp_begreen.begreen.dialog
 import android.Manifest
 import androidx.fragment.app.testing.FragmentScenario
 import androidx.fragment.app.testing.launchFragmentInContainer
-import androidx.fragment.app.viewModels
 import androidx.test.espresso.Espresso.onView
 import androidx.test.espresso.action.ViewActions.click
 import androidx.test.espresso.assertion.ViewAssertions.doesNotExist
 import androidx.test.espresso.assertion.ViewAssertions.matches
-import androidx.test.espresso.matcher.ViewMatchers.assertThat
 import androidx.test.espresso.matcher.ViewMatchers.isDisplayed
 import androidx.test.espresso.matcher.ViewMatchers.withId
 import androidx.test.espresso.matcher.ViewMatchers.withText
@@ -16,35 +14,27 @@ import androidx.test.ext.junit.runners.AndroidJUnit4
 import androidx.test.filters.LargeTest
 import androidx.test.platform.app.InstrumentationRegistry
 import androidx.test.rule.GrantPermissionRule
-import androidx.test.uiautomator.By
 import androidx.test.uiautomator.UiDevice
-import androidx.test.uiautomator.UiSelector
-import androidx.test.uiautomator.Until
 import com.github.sdp_begreen.begreen.R
 import com.github.sdp_begreen.begreen.models.CustomLatLng
-import com.github.sdp_begreen.begreen.viewModels.ContestMapDialogViewModel
-import com.google.android.gms.maps.model.Circle
-import com.google.android.gms.maps.model.Marker
-import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.channels.Channel
-import kotlinx.coroutines.launch
-import kotlinx.coroutines.test.runTest
-import org.hamcrest.CoreMatchers.equalTo
-import org.hamcrest.CoreMatchers.`is`
-import org.hamcrest.CoreMatchers.notNullValue
-import org.hamcrest.CoreMatchers.nullValue
-import org.hamcrest.Matchers.closeTo
 import org.junit.Before
 import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
-import kotlin.test.assertTrue
 
+/**
+ * Those constant are not used anymore, because they are used in the commented tests
+ *
+ * We let them there if we can uncomment those test in the future
+ */
+
+/*
 private const val MAP_INITIALIZATION_TIMEOUT = 10000L
 private val INITIAL_LOCATION = CustomLatLng(46.518078, 6.561769)
 private const val INITIAL_RADIUS = 1506.8
+*/
 
-@OptIn(ExperimentalCoroutinesApi::class)
 @RunWith(AndroidJUnit4::class)
 @LargeTest
 class ContestMapDialogTest {
@@ -119,6 +109,41 @@ class ContestMapDialogTest {
     }
 
     @Test
+    fun clickingCancelButtonCorrectlyCloseDialog() {
+        onView(withId(R.id.create_contest_map_cancel_button))
+            .check(matches(isDisplayed()))
+            .perform(click())
+
+        onView(withId(R.id.create_contest_map_layout))
+            .check(doesNotExist())
+    }
+
+    @Test
+    fun clickingApproveButtonCloseDialog() {
+        // Another tests that test more deeply the approve functionality is written below,
+        // but needed to be commented, see explanation for more details on why
+
+        onView(withId(R.id.create_contest_map_approve_button))
+            .check(matches(isDisplayed()))
+            .perform(click())
+
+        onView(withId(R.id.create_contest_map_layout))
+            .check(doesNotExist())
+    }
+
+    /**
+     * Those tests have to be commented, because they cannot run on the CI.
+     *
+     * They need the map of Google map, but for that the device need to have
+     * google_play_services enable, which is not the case in the device we have on the CI
+     *
+     * We tried to change the device on the CI, but it resulted in flaky behavior, tests were
+     * passing 40% of the time.
+     *
+     * We decided to let them here commented, if someday we find a way to run them on the CI.
+     */
+
+    /*@Test
     fun clickingApproveButtonCorrectlyNotifyListenerAndCloseDialog() {
         val args = ContestMapDialog.newInstance(
             contestMapDialogListener,
@@ -142,16 +167,6 @@ class ContestMapDialogTest {
             onView(withId(R.id.create_contest_map_layout))
                 .check(doesNotExist())
         }
-    }
-
-    @Test
-    fun clickingCancelButtonCorrectlyCloseDialog() {
-        onView(withId(R.id.create_contest_map_cancel_button))
-            .check(matches(isDisplayed()))
-            .perform(click())
-
-        onView(withId(R.id.create_contest_map_layout))
-            .check(doesNotExist())
     }
 
     @Test
@@ -266,6 +281,6 @@ class ContestMapDialogTest {
             assertThat(radiusChannel.receive(), `is`(notNullValue()))
             assertThat(circleChannel.receive(), `is`(notNullValue()))
         }
-    }
+    }*/
 
 }
